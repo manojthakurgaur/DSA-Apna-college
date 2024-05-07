9f92ccf1a55d9d8a611c06fd0b2a18fa88e1753e
Manojthakurgaur <128888244+manojthakurgaur@users.noreply.github.com>
2024-05-07 12:35:02 +0530

    Add files via upload

--- apna.cpp
+++ apna.cpp
@@ -243,7 +243,21 @@ void max_till_i_apna(int arr[],int n){
 */
 //Q2.Sum of all sub arrays.
 void sum_subarray(int arr[],int n){
-    
+    int sum=0;
+    int m=n;
+    for(int i=0;i<n;i++){
+        int l=1;
+        for(int j=0;j<m;j++){
+            for(int k=i;k<l+i;k++){
+                sum=sum+arr[k];
+            }
+            l=l+1;
+        }
+        m=m-1;
+    }
+    cout<<sum;
+
+ 
 
 }
 int main(){
